f525885bf0d123979e58b2e7890e4d76713593d4
giannis.karystinos <giannis.karystinos@gugroup.com>
2021-03-30 21:10:41 +0300

    Added Context

---
+++ src/context/BlogContext.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/*
+  Props => 1) Communicates information from a parent directyle down to child
+           2) Easy to setup
+           3) To communicate data down multiple layers, we have to write a lot of code
+
+  Context=>1)Moves information from a parent to some nested child
+           2)Complicated to setup, lots of special terms
+           3)Easy to communicate data from a parent to a super nested child         
+*/
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({children}) =>{
+    return <BlogContext.Provider>
+            {children}
+    </BlogContext.Provider>
+}
+
